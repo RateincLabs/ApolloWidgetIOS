03adacf98a8685ba9847dee139cc7654ce68836c
dnlsgv <segoviadaniel2@gmail.com>
2023-01-22 22:41:31 -0300

    stuff

---
+++ ApolloWidget.xcframework/ios-arm64_arm64e/ApolloWidget.framework/Headers/ApolloWidget.h
@@ -0,0 +1,18 @@
+//
+//  ApolloWidget.h
+//  ApolloWidget
+//
+//  Created by Ernesto Perez on 09-01-22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ApolloWidget.
+FOUNDATION_EXPORT double ApolloWidgetVersionNumber;
+
+//! Project version string for ApolloWidget.
+FOUNDATION_EXPORT const unsigned char ApolloWidgetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ApolloWidget/PublicHeader.h>
+
+
